32d6c5006864ce9b867f0baf5e166b4e250ee408
major <majorkiema.mk@gmail.com>
2023-05-31 21:39:39 +0300

    fixed Lambda code

--- finalproject/lambda.py
+++ finalproject/lambda.py
@@ -2,29 +2,34 @@ import boto3
 
 def lambda_handler(event, context):
     # Extract relevant information from the DynamoDB trigger event
-    user_email = event['Records'][0]['dynamodb']['NewImage']['email']['S']
-    # ... Extract any other necessary information ...
+    for record in event['Records']:
+        if record['eventName'] == 'INSERT':
+            new_image = record['dynamodb']['NewImage']
+            user_email = new_image['Email']['S']
+            # ... Extract any other necessary information ...
 
-    # Create an SES client
-    ses = boto3.client('ses')
+            # Create an SES client
+            ses = boto3.client('ses')
 
-    # Send an email to the new subscriber
-    response = ses.send_email(
-        Source='majorkiema.mk@gmail.com',  # Replace with your verified email address
-        Destination={
-            'ToAddresses': [user_email]
-        },
-        Message={
-            'Subject': {
-                'Data': 'Welcome to Our Service'
-            },
-            'Body': {
-                'Text': {
-                    'Data': 'Hello, thank you for Registering with US!'
+            # Send an email to the new subscriber
+            response = ses.send_email(
+                Source='verified_email',  # Replace with your verified email address
+                Destination={
+                    'ToAddresses': [user_email]
+                },
+                Message={
+                    'Subject': {
+                        'Data': 'Welcome to Our Service'
+                    },
+                    'Body': {
+                        'Text': {
+                            'Data': 'Thank you for filling out the form. Welcome to the Gold Grid family!'
+                        }
+                    }
                 }
-            }
-        }
-    )
+            )
 
-    return 'Email sent successfully: ' + response['MessageId']
+            print('Email sent successfully: ' + response['MessageId'])
+
+
 
